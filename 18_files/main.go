e03443883eae6844b065ae9937183e73b32877c8
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-08-15 17:50:52 +0530

    Handling Files in Golang

---
+++ 18_files/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Files in Golang")
+	content := "This needs to go in a file - https://github.com/0xDarkXnight"
+
+	file, err := os.Create("./darkxnight.txt")
+	checkNilErr(err)
+
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+	fmt.Println("The length is:", length)
+
+	defer file.Close()
+	readFile("./darkxnight.txt")
+}
+
+func readFile(fileName string) {
+	databyte, err := ioutil.ReadFile(fileName)
+	checkNilErr(err)
+	fmt.Println("Text data inside the file is:", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
