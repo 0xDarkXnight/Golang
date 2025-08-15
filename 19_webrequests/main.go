a9043d45f12f2d62ffa8573545c8570102753548
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-08-15 16:34:18 +0000

    WebRequests in Golang

---
+++ 19_webrequests/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm"
+
+func main() {
+	fmt.Println("WebRequests in Golang")
+
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("The Response if of Type: %T\n", response)
+
+	defer response.Body.Close() // caller's responsibility to close the connection
+
+	databytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+	fmt.Println(content)
+}
