b1f92845c65d9b9d7eeba7a15d2258d08ec548c6
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-08-16 13:47:13 +0530

    WebReqVerbs in Golang

---
+++ 21_webreqverbs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("WebRequestVerbs in Golang")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code:", response.StatusCode)
+	fmt.Println("Content Length is:", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+
+	fmt.Println("ByteCount is:", byteCount)
+	fmt.Println(responseString.String())
+
+	fmt.Println(content)
+	fmt.Println(string(content))
+}
